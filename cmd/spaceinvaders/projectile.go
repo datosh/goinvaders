41211f25eca1d6a45a88923052e4419a20cb80d7
Fabian Kammel <datosh@gmx.de>
2020-05-26 21:01:03 +0200

    Ohhh dang! Enemies shoot back!

--- cmd/spaceinvaders/projectile.go
+++ cmd/spaceinvaders/projectile.go
@@ -9,19 +9,19 @@ import (
 
 type Projectile struct {
 	*engine.Entity
-	speed float64
+	Direction *vec2.T
 }
 
-func NewProjectile(position *vec2.T) *Projectile {
+func NewProjectile(position, direction *vec2.T) *Projectile {
 	projectile := &Projectile{
-		Entity: engine.NewEntity(),
+		Entity:    engine.NewEntity(),
+		Direction: direction,
 	}
 	projectile.Image = engine.LoadSubImage(
 		"/img/spritemap.png",
 		engine.CoordinatesToBounds(vec2.I{64, 48}, vec2.I{2, 2}),
 	)
 	projectile.Position = position
-	projectile.speed = 5.0
 	projectile.ImageScale = 0.25
 	projectile.HitboxSize = vec2.NewI(projectile.Image.Size()).AsT().
 		Mul(projectile.ImageScale)
@@ -30,7 +30,7 @@ func NewProjectile(position *vec2.T) *Projectile {
 
 func (p *Projectile) Update(screen *ebiten.Image) error {
 	p.Entity.Update(screen)
-	p.Position.Add(vec2.UY().Mul(p.speed).Invert())
+	p.Position.Add(p.Direction)
 	return nil
 }
 
